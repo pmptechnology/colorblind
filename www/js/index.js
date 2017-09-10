74c129224627d88a6a28696e548db4dd01af9cba
Philip Parzygnat <philipparzygnat@PMP-Mac-mini.local>
2017-09-09 20:57:57 -0400

    Minor Update

--- www/js/index.js
+++ www/js/index.js
@@ -44,26 +44,3 @@ var app = {
 };
 
 app.initialize();
-
-function openFilePicker(selection) {
-
-    var srcType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
-    var options = setOptions(srcType);
-    var func = createNewFileEntry;
-
-    if (selection == "picker-thmb") {
-        // To downscale a selected image,
-        // Camera.EncodingType (e.g., JPEG) must match the selected image type.
-        options.targetHeight = 100;
-        options.targetWidth = 100;
-    }
-
-    navigator.camera.getPicture(function cameraSuccess(imageUri) {
-
-        // Do something with image
-
-    }, function cameraError(error) {
-        console.debug("Unable to obtain picture: " + error, "app");
-
-    }, options);
-}
